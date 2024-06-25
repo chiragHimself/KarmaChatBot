b11248cc0ad8e8cee0736df2b1b8b1e8c3488268
Chirag <118044484+chiragHimself@users.noreply.github.com>
2024-06-25 22:21:34 +0530

    Update baserunstphi.py

--- baserunstphi.py
+++ baserunstphi.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 from langchain_community.vectorstores import Chroma
-from langchain.document_loaders import TextLoader, DirectoryLoader
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 import ollama
