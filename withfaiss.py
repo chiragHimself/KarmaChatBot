7fee86ec880fce1c715812f7cd1df3a68feab6d2
Chirag <118044484+chiragHimself@users.noreply.github.com>
2024-06-26 16:13:07 +0530

    Update withfaiss.py

--- withfaiss.py
+++ withfaiss.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 #from langchain_community.vectorstores import Chroma
 from langchain_community.vectorstores import FAISS
-from langchain.document_loaders import TextLoader, DirectoryLoader
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 import ollama
